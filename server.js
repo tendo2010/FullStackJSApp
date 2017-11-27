9f60275e27ec9142e428c7a6edbcd88a111789dd
Nawapong  Atiwattananon <tendo2010@users.noreply.github.com>
2017-11-27 12:34:56 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,67 +1,37 @@
-var express = require('express');
+var express = require('express');        
+var app = express();   
+var cors = require('cors');              
 var bodyParser = require('body-parser');
-var app = express();
-var users = require('./users.js');
-var cors = require('cors');
-var path = require('path');
+var posts = require('./post.js');
+var moment = require('moment');
+moment.locale('th');
+// POINT 1. Enable CORS
 app.use(cors());
-app.use(express.static('www'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
 
-function hello(req, res, next) {
-    res.write("Hello \n");
-    next();
-}
 
-function now(req, res, next) {
-    var now = new Date();
-    res.write('Now: ' + now + "\n");
-    next();
-}
+// POINT 2. Set a static file for “frontend” folder
+app.use(express.static(__dirname + "/frontend"));
 
-function goodbye(req, res, next) {
-    res.write('Goodbye \n');
-    res.end();
-}
+var port = process.env.PORT || 8080;        // set our port
 
-function logger(req, res, next) {
-    console.log(new Date(), req.method, req.url);
-    next();
-}
+var router = express.Router();              // get an instance of the express Router
 
-app.use(logger);
 
-app.get('/hello', hello, now, goodbye);
 
-app.get('/', function (request, response) {
-    response.send('Hello World');
-});
+// POINT 3. Set API routing to functions in post.js
+router.get('/posts', posts.getAllPosts);
+router.get('/users/posts', posts.getPostsByUser);
+router.post('/posts', posts.insertNewPosts);
 
-app.get('/now', function (request, response) {
-    var now = new Date();
-    response.send('<strong>Now:</strong> ' + now);
-});
 
-app.get('/json', function (request, response) {
-    var user = {
-        id: 111,
-        name: "User111"
-    };
-    response.send(user);
-});
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', cors(), router);
 
-app.post('/api/users', function (req, res) {
-    var user_id = req.body.id;
-    var token = req.body.token;
-    var geo = req.body.geo;
-    res.send(user_id + ' ' + token + ' ' + geo);
-});
-
-app.get('/users', users.findAll);
-app.get('/users/search', users.findByFname)
-app.get('/users/role/:role', users.findByRole);
-
-app.listen(3000, () => console.log('Server is running at http://localhost:3000'));
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
